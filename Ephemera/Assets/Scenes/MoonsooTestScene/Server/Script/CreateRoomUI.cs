3220087de2b549525098b3f6b3b051e83f44c2c8
Ludning <94681711+Ludning@users.noreply.github.com>
2024-05-10 08:21:57 +0900

    서버 연구

---
+++ Ephemera/Assets/Scenes/MoonsooTestScene/Server/Script/CreateRoomUI.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateRoomUI : MonoBehaviour
+{
+    public void CreateRoom()
+    {
+        var manager = EphemeraRoomManager.singleton;
+
+        //방 설정 작업 처리
+        //TODO
+
+        //서버를 여는 동시에 클라이언트로써 게임에 참가할 수 있도록 만들어주는 함수
+        manager.StartHost();
+
+        //클라이언트로써 게임에 참가할 수 있도록 만들어주는 함수
+        //manager.StartClient();
+    }
+}
