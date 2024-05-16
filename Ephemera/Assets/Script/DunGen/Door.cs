3d6b195512dbf4214d37510326e042a9203477d1
K-Byeonggon <govleen79@gmail.com>
2024-05-16 19:23:21 +0900

    [Update] 열고 닫을 수 있는 문 추가, 행동 트리 공부

---
+++ Ephemera/Assets/Script/DunGen/Door.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private bool isOpen;
+    private Animator animator;
+
+    private void OnEnable()
+    {
+        animator = transform.parent.GetComponent<Animator>();
+    }
+
+    //플레이어에서 Ray를 쏴서 E를 누르면 GetComponent<Door>().UseDoor()하면 되지 않을까?
+    public void UseDoor()
+    {
+        if (!isOpen) { OpenDoor(); }
+        else { CloseDoor(); }
+    }
+
+    public void OpenDoor()
+    {
+        isOpen = true;
+        animator.SetBool("IsOpen", true);
+    }
+
+    public void CloseDoor() 
+    { 
+        isOpen = false;
+        animator.SetBool("IsOpen", false);    
+    }
+
+}
