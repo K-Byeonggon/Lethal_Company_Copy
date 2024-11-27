7775f9ec394b615c9f794cc8b8984d1e82b63f86
K-Byeonggon <govleen79@gmail.com>
2024-11-27 14:55:57 +0900

    [feat] 노드의 Running 상태 동작 수정

--- Ephemera/Assets/Script/Monster/BehaviorTree/SelectorNode.cs
+++ Ephemera/Assets/Script/Monster/BehaviorTree/SelectorNode.cs
@@ -3,25 +3,33 @@ using System.Collections.Generic;
 //children의 Node중 하나만 실행해야 할때 사용
 public class SelectorNode : CompositeNode
 {
+    //부모인 CompositeNode의 생성자 호출
     public SelectorNode(List<Node> children) : base(children) { }
 
+    //자식 노드들을 평가하고, 하나라도 성공하면 성공을 반환한다.
     public override State Evaluate()
     {
-        foreach (var child in children)
+        int startIndex = runningNodeIndex != -1 ? runningNodeIndex : 0;
+
+        for (int i = startIndex; i < children.Count; i++)
         {
-            switch (child.Evaluate())
+            switch (children[i].Evaluate())
             {
                 case State.RUNNING:
-                    state = State.RUNNING;
+                    runningNodeIndex = i;   //현재 노드를 기억
+                    state = State.RUNNING;  //셀렉터도 RUNNING이 된다.
                     return state;
                 case State.SUCCESS:
-                    state = State.SUCCESS;
+                    runningNodeIndex = -1;  // 성공시 RUNNING 인덱스 초기화
+                    state = State.SUCCESS;  //셀렉터가 SUCCESS 반환
                     return state;
                 case State.FAILURE:
-                    continue;
+                    continue;               //실패시 다음 노드 평가
             }
         }
-        state = State.FAILURE;
+
+        runningNodeIndex = -1;  //모든 노드 실패시 인덱스 초기화
+        state = State.FAILURE;  //셀렉터가 FAILURE 반환
         return state;
     }
 }
--- Ephemera/Assets/Script/Monster/BehaviorTree/SequenceNode.cs
+++ Ephemera/Assets/Script/Monster/BehaviorTree/SequenceNode.cs
@@ -3,28 +3,32 @@ using System.Collections.Generic;
 //children의 Node를 순서대로 실행해야 할때 사용
 public class SequenceNode : CompositeNode
 {
+    //부모인 CompositeNode의 생성자 호출
     public SequenceNode(List<Node> children) : base(children) { }
 
     public override State Evaluate()
     {
-        bool anyChildRunning = false;
+        int startIndex = runningNodeIndex != -1 ? runningNodeIndex : 0;
 
-        foreach (var child in children)
+        for (int i = startIndex; i < children.Count; i++)
         {
-            switch (child.Evaluate())
+            switch (children[i].Evaluate())
             {
                 case State.RUNNING:
-                    state = State.RUNNING;
+                    runningNodeIndex = i;       //현재 노드를 기억
+                    state = State.RUNNING;      //시퀀스도 RUNNING이 된다.
                     return state;
                 case State.SUCCESS:
-                    continue;
+                    continue;                   //성공시 다음 노드 평가
                 case State.FAILURE:
-                    state = State.FAILURE;
+                    runningNodeIndex = -1;      //실패시 Running노드 초기화
+                    state = State.FAILURE;      //시퀀스가 FAILURE 반환
                     return state;
             }
         }
 
-        state = anyChildRunning ? State.RUNNING : State.SUCCESS;
+        runningNodeIndex = -1;      //인덱스 초기화
+        state = State.SUCCESS;      //모든 노드가 성공하면 성공 반환
         return state;
     }
 }
